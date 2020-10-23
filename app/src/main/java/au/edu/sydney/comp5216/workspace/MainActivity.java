af22ee21406c024adf774f934fd5845a982124a6
xiangyunxiang <xiangyunxiang@protonmail.com>
2020-10-23 22:46:49 +0800

    intial commit

---
+++ app/src/main/java/au/edu/sydney/comp5216/workspace/MainActivity.java
@@ -0,0 +1,30 @@
+package au.edu.sydney.comp5216.workspace;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.meal);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
